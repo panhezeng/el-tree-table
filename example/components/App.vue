39cfcdf1a785b7494ff3f836aeb8294eee4ef96b
Pan Hezeng <panhezeng@gmail.com>
2018-12-28 21:18:46 +0800

    懒加载节点功能

--- example/components/App.vue
+++ example/components/App.vue
@@ -1,88 +1,125 @@
 <template>
   <div id="app">
-    <el-tree-table :tree-data="treeData" :columns="columns" style="width: 100%"/>
+    <el-tree-table ref="treeTable" :expand-unique-values="expandUniqueValues" :tree-data="treeData" :columns="columns" style="width: 100%" @init-data="initData" @toggle-expand="toggleExpand"/>
   </div>
 </template>
 
 <script>
+// import '../../dist/el-tree-table.min.js'
+//  import Vue from 'vue'
+//  import ElTreeTable from '../../dist/el-tree-table.min.js'
+import ElTreeTable from "../../src/ElTreeTable.vue";
+//  const ElTreeTable = require('../../dist/el-tree-table.min.js')
 
-  // import '../../dist/el-tree-table.min.js'
-  //  import Vue from 'vue'
-  //  import ElTreeTable from '../../dist/el-tree-table.min.js'
-   import ElTreeTable from '../../src/ElTreeTable.vue'
-  //  const ElTreeTable = require('../../dist/el-tree-table.min.js')
+//  Vue.use(ElTreeTable)
 
-  //  Vue.use(ElTreeTable)
+//  const ElTreeTable = () => import('../../dist/el-tree-table.min.js')
 
-  //  const ElTreeTable = () => import('../../dist/el-tree-table.min.js')
-
-  export default {
-    name: 'App',
-   components: {ElTreeTable},
-    data() {
-      return {
-        columns: [
-          {
-            prop: 'id',
-            label: 'ID',
-            width: 65
-          },
-          {
-            prop: 'title',
-            label: '标题',
-          }
-        ],
-        treeData: [
-          {
-            id: 1,
-            title: '标题1-1',
-          },
-          {
-            id: 2,
-            title: '标题1-2',
-            children: [
-              {
-                id: 3,
-                title: '标题1-2-1',
-              },
-              {
-                id: 4,
-                title: '标题1-2-2',
-                children: [
-                  {
-                    id: 5,
-                    title: '标题1-2-2-1',
-                  },
-                  {
-                    id: 6,
-                    title: '标题1-2-2-2',
-                  },
-                ]
-              },
-              {
-                id: 7,
-                title: '标题1-2-3',
-              },
-              {
-                id: 8,
-                title: '标题1-2-4',
-                children: [
-                  {
-                    id: 9,
-                    title: '标题1-2-4-1',
-                  },
-                  {
-                    id: 10,
-                    title: '标题1-2-4-2',
-                  },
-                ]
-              }
-            ]
-          }
-        ]
-      }
+export default {
+  name: "App",
+  components: { ElTreeTable },
+  data() {
+    return {
+      columns: [
+        {
+          prop: "id",
+          label: "ID",
+          width: 65
+        },
+        {
+          prop: "title",
+          label: "标题",
+          "column-key": "title",
+          filters: [
+            { text: "标题1-2-1", value: "标题1-2-1" },
+            { text: "标题1-2-2", value: "标题1-2-2" }
+          ],
+          "filter-method": this.filterHandler
+        }
+      ],
+      expandUniqueValues: [],
+      treeData: [
+        {
+          id: 1,
+          title: "标题1-1"
+        },
+        {
+          id: 2,
+          title: "标题1-2",
+          children: [
+            {
+              id: 3,
+              title: "标题1-2-1"
+            },
+            {
+              id: 4,
+              title: "标题1-2-2",
+              children: [
+                {
+                  id: 5,
+                  title: "标题1-2-2-1"
+                },
+                {
+                  id: 6,
+                  title: "标题1-2-2-2"
+                }
+              ]
+            },
+            {
+              id: 7,
+              title: "标题1-2-3"
+            },
+            {
+              id: 8,
+              title: "标题1-2-4",
+              children: [
+                {
+                  id: 9,
+                  title: "标题1-2-4-1"
+                },
+                {
+                  id: 10,
+                  title: "标题1-2-4-2"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: 11,
+          title: "标题1-3",
+          children_count: 1
+        }
+      ]
+    };
+  },
+  methods: {
+    filterHandler(value, row, column) {
+      const property = column["property"];
+      return row[property] === value;
     },
-    methods: {}
+    initData(data) {
+      this.tableData = data;
+    },
+    toggleExpand(row) {
+      // 如果是展开，并且还没孩子节点数据，则加载
+      if (row.treeExpand && !(row.children && row.children.length)) {
+        console.log(row, 11111);
+        setTimeout(() => {
+          row.children = [
+            {
+              id: 12,
+              title: "标题1-3-1"
+            }
+          ];
+          this.expandUniqueValues = this.$refs.treeTable
+            .getExpandRows()
+            .map(value => value.id)
+            .concat([12]);
+          this.treeData = this.treeData.slice();
+        }, 1000);
+      }
+    }
   }
+};
 </script>
-
--- src/ElTreeTable.vue
+++ src/ElTreeTable.vue
@@ -7,7 +7,7 @@
     <el-table-column
       type="selection"
       :width="columnSelectionWidth" v-if="columnSelectionWidth"/>
-    <el-table-column :width="columnExpandWidth" v-if="columnExpand">
+    <el-table-column :width="columnExpandWidth" column-key="columnExpand" v-if="columnExpand">
       <template slot-scope="scope">
         <div :style="`margin-left: ${scope.row.treeLevel * columnExpandIndent}px;`">
           <span @click="toggleExpand(scope.row,scope.$index)" v-if="scope.row.treeHasChildren">
@@ -20,222 +20,281 @@
     </el-table-column>
     <template v-if="columns.length">
       <el-table-column v-for="column in columns" :key="column.prop"
-                       :filtered-value="column.filteredValue"
-                       :filter-method="column.filterMethod"
-                       :filter-multiple="column.filterMultiple"
-                       :filters="column.filters"
-                       :column-key="column.prop"
-                       :prop="column.prop"
-                       :label="column.label"
-                       :width="column.width"/>
+                       v-bind="column"/>
     </template>
     <slot/>
+
+
   </el-table>
 </template>
 <script>
-  import {Table, TableColumn} from "element-ui";
+import { Table, TableColumn } from "element-ui";
 
-  // 展开操作列的初始宽度
-  const columnExpandWidthInit = 55;
+// 展开操作列的初始宽度
+const columnExpandWidthInit = 55;
 
-  export default {
-    name: "ElTreeTable",
-    components: {"el-table": Table, "el-table-column": TableColumn},
-    inheritAttrs: false,
-    props: {
-      // 数据的唯一标识key
-      uniqueKey: {type: String, default: "id"},
-      // 获取树形数据的子节点的key
-      treeChildrenKey: {type: String, default: "children"},
-      // 树形数据结构，用来转换成表格数据
-      treeData: {},
-      // 表格数据结构，如果传了这个，就不用treeData了
-      // tableData的row数据对象必须有row.treeFullIndex，row.treeLevel, row.treeHasChildren属性，而且值必须正确有效
-      tableData: {},
-      // 是否展开所有
-      expandAll: {
-        type: Boolean,
-        default: false,
-      },
-      // 勾选列宽，为0，则不显示
-      columnSelectionWidth: {
-        type: Number,
-        default: 55
-      },
-      // 选择父子关联
-      selectionRelate: {
-        type: Boolean,
-        default: true
-      },
-      // 展开操作列
-      columnExpand: {
-        type: Boolean,
-        default: true
-      },
-      // 展开操作缩进像素
-      columnExpandIndent: {
-        type: Number,
-        default: 20
-      },
-      // 要渲染的列数据，没有则不渲染，开发者可以通过slot，自己实现
-      columns: {
-        type: Array,
-        default() {
-          return []
-        }
-      },
-      expandIcon: {
-        type: String,
-        default: 'el-icon-caret-bottom'
-      },
-      collapseIcon: {
-        type: String,
-        default: 'el-icon-caret-right'
-      },
-      leafIcon: {
-        type: String,
-        default: 'el-icon-minus'
+export default {
+  name: "ElTreeTable",
+  components: { "el-table": Table, "el-table-column": TableColumn },
+  inheritAttrs: false,
+  props: {
+    // 每行数据的唯一标识key
+    uniqueKey: { type: String, default: "id" },
+    // 获取树形数据的子节点的key
+    treeChildrenKey: { type: String, default: "children" },
+    // 孩子数据数量的key，是否显示展开按钮，通过treeChildrenKey和treeChildrenCountKey一起判断，
+    // 如果通过treeChildrenKey获得的数组为空，而通过treeChildrenCountKey获得为数量大于0，则也会显示展开按钮，结合toggle-expand事件可实现懒加载
+    // 懒加载获得的子节点数据后，更新treeData前，通过getExpandRows可以获得所有已展开的数据，结合expandUniqueKeys，实现更新treeData后还原展开状态
+    treeChildrenCountKey: { type: String, default: "children_count" },
+    // 树形数据结构，用来转换成表格数据
+    treeData: {},
+    // 表格数据结构，如果传了这个，就不用treeData了
+    // tableData的row数据对象必须有row.treeFullIndex，row.treeLevel, row.treeHasChildren属性，而且值必须正确有效
+    tableData: {},
+    // 是否展开所有
+    expandAll: {
+      type: Boolean,
+      default: false
+    },
+    // 默认需要展开的行，数组值是每行的uniqueValue
+    expandUniqueValues: {
+      type: Array,
+      default() {
+        return [];
       }
     },
-    data() {
-      return {
-        data: [],
-        columnExpandWidth: columnExpandWidthInit
-      };
+    // 勾选列宽，为0，则不显示
+    columnSelectionWidth: {
+      type: Number,
+      default: 55
     },
-    watch: {
-      treeData: {
-        handler() {
-          this.initData();
-        },
-        immediate: true,
-      },
-      tableData: {
-        handler() {
-          this.initData();
-        },
-        immediate: true,
-      },
+    // 选择父子关联
+    selectionRelate: {
+      type: Boolean,
+      default: true
     },
-    methods: {
-      initData() {
-        if (Object.prototype.toString.call(this.treeData) === "[object Array]" && this.treeData.length) {
-          this.data = this.treeToTableData(JSON.parse(JSON.stringify(this.treeData)))
-        } else if (Object.prototype.toString.call(this.tableData) === "[object Array]" && this.tableData.length) {
-          this.data = JSON.parse(JSON.stringify(this.tableData));
-        }
-      },
-      /**
-       * 把树形嵌套数据结构转换为表格并列数据结构，并且增加属性标识每条数据对象之间的树形关系
-       * @param from
-       * @param to
-       * @param fullIndex 当前节点的完整索引，包括祖先节点的索引
-       * @param level 当前递归的层级
-       * @return {Array}
-       */
-      treeToTableData(from, to = [], fullIndex = "", level = 0) {
-        if (
-          Object.prototype.toString.call(from) === "[object Array]" &&
-          from.length
-        ) {
-          for (let i = 0, len = from.length; i < len; i++) {
-            const node = from[i];
-            // 表格一行的数据
-            const row = JSON.parse(JSON.stringify(node));
-            row.rowIndex = to.length;
-            to.push(row);
-            // 第一层初始化
-            if (level === 0) {
-              fullIndex = "";
-              row.rowShow = true;
-            } else {
-              // 如果是第二层以后非第一个元素，则需要去掉前一个兄弟元素
-              if (i) {
-                const fullIndexArray = String(fullIndex).split("-");
-                fullIndexArray.pop();
-                fullIndex = fullIndexArray.join("-") + "-";
-              }
-              row.rowShow = this.expandAll;
-            }
-            // 节点的唯一标识
-            fullIndex = `${fullIndex}${i}`;
-            row.treeFullIndex = fullIndex;
-            row.treeLevel = level;
-            row.treeExpand = this.expandAll;
-            row.treeHasChildren = false;
-            const children = node[this.treeChildrenKey];
-            if (
-              Object.prototype.toString.call(children) === "[object Array]" &&
-              children.length
-            ) {
-              row.treeHasChildren = true;
-              delete row[this.treeChildrenKey];
-              this.treeToTableData(
-                children,
-                to,
-                `${fullIndex}-`,
-                level + 1,
-              );
-            }
-          }
-        }
-        return to
-      },
-      getRowStyle({row}) {
-        return row.rowShow === false ? 'display:none;' : ''
-      },
-      toggleExpand: function (clickRow) {
-        clickRow.treeExpand = !clickRow.treeExpand;
-        let maxLevelShow = clickRow.treeLevel;
-        let continueTest = null;
-        // 必须从上到下遍历，不然通过正则匹配处理子节点row的逻辑无法实现
-        for (let i = 0, len = this.data.length; i < len; i++) {
-          const row = this.data[i];
-          // 如果遍历的行不是点击的行，则继续
-          if (row[this.uniqueKey] !== clickRow[this.uniqueKey]) {
-            // 如果有跳过检查，并且遍历的行满足条件，则执行跳过
-            // 否则如果遍历的行是当前点击行的子节点
-            if (continueTest && continueTest.test(row.treeFullIndex)) {
-              continue;
-            } else if (new RegExp(`^${clickRow.treeFullIndex}`).test(row.treeFullIndex)) {
-              row.rowShow = clickRow.treeExpand;
-
-              continueTest = null;
-              // 如果当前点击是展开，并且下一行有子节点，而且是不展开，则它后面的子节点都跳过
-              if (clickRow.treeExpand && row.treeHasChildren && !row.treeExpand) {
-                continueTest = new RegExp(`^${row.treeFullIndex}`)
-              }
-
-            }
-            // 获得显示行的最大层级
-            if (row.rowShow && row.treeLevel > maxLevelShow) {
-              maxLevelShow = row.treeLevel;
-            }
-          }
-        }
-
-        this.columnExpandWidth = columnExpandWidthInit + (maxLevelShow * this.columnExpandIndent)
-      },
-      toggleRowSelection(clickRow, selected) {
-        // 必须从上到下遍历，不然通过正则匹配处理子节点row的逻辑无法实现
-        for (let i = clickRow.rowIndex + 1, len = this.data.length; i < len; i++) {
-          const row = this.data[i];
-          if (new RegExp(`^${clickRow.treeFullIndex}`).test(row.treeFullIndex)) {
-            this.$refs.treeTable.toggleRowSelection(row, selected);
-          } else {
-            break;
-          }
-        }
-      },
-      handleSelect(selection, clickRow) {
-        if(this.selectionRelate){
-          // 如果为真则是勾选
-          // 否则是取消勾选
-          this.toggleRowSelection(clickRow, selection.some(row => row[this.uniqueKey] === clickRow[this.uniqueKey]))
-        }
-        this.$emit('select', selection, clickRow)
+    // 展开操作按钮列
+    columnExpand: {
+      type: Boolean,
+      default: true
+    },
+    // 展开操作缩进像素
+    columnExpandIndent: {
+      type: Number,
+      default: 20
+    },
+    // 要渲染的列数据，没有则不渲染，开发者可以通过slot，自己实现
+    columns: {
+      type: Array,
+      default() {
+        return [];
       }
+    },
+    expandIcon: {
+      type: String,
+      default: "el-icon-caret-bottom"
+    },
+    collapseIcon: {
+      type: String,
+      default: "el-icon-caret-right"
+    },
+    leafIcon: {
+      type: String,
+      default: "el-icon-minus"
     }
-  };
-</script>
+  },
+  data() {
+    return {
+      data: [],
+      columnExpandWidth: columnExpandWidthInit
+    };
+  },
+  watch: {
+    treeData: {
+      handler() {
+        this.initData();
+      },
+      immediate: true
+    },
+    tableData: {
+      handler() {
+        this.initData();
+      },
+      immediate: true
+    }
+  },
+  methods: {
+    expand(row) {
+      return (
+        this.expandAll ||
+        this.expandUniqueValues.indexOf(row[this.uniqueKey]) !== -1
+      );
+    },
+    getExpandRows() {
+      if (
+        Object.prototype.toString.call(this.data) === "[object Array]" &&
+        this.data.length
+      ) {
+        return this.data.filter(row => row.treeExpand);
+      }
+      return [];
+    },
+    initData() {
+      if (
+        Object.prototype.toString.call(this.treeData) === "[object Array]" &&
+        this.treeData.length
+      ) {
+        this.data = this.treeToTableData(this.treeData);
+      } else if (
+        Object.prototype.toString.call(this.tableData) === "[object Array]" &&
+        this.tableData.length
+      ) {
+        this.data = this.tableData;
+      }
+      this.$emit("init-data", this.data);
+    },
+    /**
+     * 把树形嵌套数据结构转换为表格并列数据结构，并且增加属性标识每条数据对象之间的树形关系
+     * @param from
+     * @param to
+     * @param fullIndex 当前节点的完整索引，包括祖先节点的索引
+     * @param level 当前递归的层级
+     * @param maxLevelShow 显示的最大层级
+     * @return {Array}
+     */
+    treeToTableData(
+      from,
+      to = [],
+      fullIndex = "",
+      level = 0,
+      maxLevelShow = 0
+    ) {
+      if (
+        Object.prototype.toString.call(from) === "[object Array]" &&
+        from.length
+      ) {
+        for (let i = 0, len = from.length; i < len; i++) {
+          const row = from[i];
+          // 表格一行的数据
+          // const row = JSON.parse(JSON.stringify(node));
+          this.$set(row, "rowIndex", to.length);
+          to.push(row);
+          const treeExpand = this.expand(row);
+          // 第一层初始化
+          if (level === 0) {
+            fullIndex = "";
+            this.$set(row, "rowShow", true);
+          } else {
+            // 如果是第二层以后非第一个元素，则需要去掉前一个兄弟元素
+            if (i) {
+              const fullIndexArray = String(fullIndex).split("-");
+              fullIndexArray.pop();
+              fullIndex = fullIndexArray.join("-") + "-";
+            }
+            this.$set(row, "rowShow", treeExpand);
+          }
+          // 节点的唯一标识
+          fullIndex = `${fullIndex}${i}`;
+          this.$set(row, "treeFullIndex", fullIndex);
+          this.$set(row, "treeLevel", level);
+          this.$set(row, "treeExpand", treeExpand);
+          this.$set(
+            row,
+            "treeHasChildren",
+            Boolean(row[this.treeChildrenCountKey])
+          );
+          // 获得显示行的最大层级
+          if (level > maxLevelShow && treeExpand) {
+            maxLevelShow = level;
+            this.columnExpandWidth =
+              columnExpandWidthInit + maxLevelShow * this.columnExpandIndent;
+          }
+          const children = row[this.treeChildrenKey];
+          if (
+            Object.prototype.toString.call(children) === "[object Array]" &&
+            children.length
+          ) {
+            this.$set(row, "treeHasChildren", true);
+            // delete row[this.treeChildrenKey];
+            this.treeToTableData(
+              children,
+              to,
+              `${fullIndex}-`,
+              level + 1,
+              maxLevelShow
+            );
+          }
+        }
+      }
+      return to;
+    },
+    getRowStyle({ row }) {
+      return row.rowShow === false ? "display:none;" : "";
+    },
+    toggleExpand: function(clickRow) {
+      this.$set(clickRow, "treeExpand", !clickRow.treeExpand);
+      let maxLevelShow = clickRow.treeLevel;
+      let continueTest = null;
+      // 必须从上到下遍历，不然通过正则匹配处理子节点row的逻辑无法实现
+      for (let i = 0, len = this.data.length; i < len; i++) {
+        const row = this.data[i];
+        // 如果遍历的行不是点击的行，则继续
+        if (row[this.uniqueKey] !== clickRow[this.uniqueKey]) {
+          // 如果有跳过检查，并且遍历的行满足条件，则执行跳过
+          // 否则如果遍历的行是当前点击行的子节点
+          if (continueTest && continueTest.test(row.treeFullIndex)) {
+            continue;
+          } else if (
+            new RegExp(`^${clickRow.treeFullIndex}`).test(row.treeFullIndex)
+          ) {
+            this.$set(row, "rowShow", clickRow.treeExpand);
 
+            continueTest = null;
+            // 如果当前点击是展开，并且下一行有子节点，而且是不展开，则它后面的子节点都跳过
+            if (clickRow.treeExpand && row.treeHasChildren && !row.treeExpand) {
+              continueTest = new RegExp(`^${row.treeFullIndex}`);
+            }
+          }
+          // 获得显示行的最大层级
+          if (row.treeLevel > maxLevelShow && row.rowShow) {
+            maxLevelShow = row.treeLevel;
+          }
+        }
+      }
+
+      this.columnExpandWidth =
+        columnExpandWidthInit + maxLevelShow * this.columnExpandIndent;
+
+      this.$emit("toggle-expand", clickRow);
+    },
+    toggleRowSelection(clickRow, selected) {
+      // 必须从上到下遍历，不然通过正则匹配处理子节点row的逻辑无法实现
+      for (
+        let i = clickRow.rowIndex + 1, len = this.data.length;
+        i < len;
+        i++
+      ) {
+        const row = this.data[i];
+        if (new RegExp(`^${clickRow.treeFullIndex}`).test(row.treeFullIndex)) {
+          this.$refs.treeTable.toggleRowSelection(row, selected);
+        } else {
+          break;
+        }
+      }
+    },
+    handleSelect(selection, clickRow) {
+      if (this.selectionRelate) {
+        // 如果为真则是勾选
+        // 否则是取消勾选
+        this.toggleRowSelection(
+          clickRow,
+          selection.some(
+            row => row[this.uniqueKey] === clickRow[this.uniqueKey]
+          )
+        );
+      }
+      this.$emit("select", selection, clickRow);
+    }
+  }
+};
+</script>
