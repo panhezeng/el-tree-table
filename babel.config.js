1bf8366c0dcad6824f2c653485e87f7b86f16711
Pan Hezeng <panhezeng@gmail.com>
2019-07-24 19:38:16 +0800

    update

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  presets: [["@babel/preset-env"]],
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: false
+      }
+    ]
+  ],
   plugins: [
     "@babel/plugin-syntax-dynamic-import",
     "@babel/plugin-transform-runtime"
